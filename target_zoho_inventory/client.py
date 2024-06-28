e93e3f77758ab246597076702cbcc6609d057683
xacadil <92389481+xacadil@users.noreply.github.com>
2024-06-28 09:43:08 -0600

    Paginated search organization fix (#10)

--- target_zoho_inventory/client.py
+++ target_zoho_inventory/client.py
@@ -106,6 +106,8 @@ class ZohoInventorySink(HotglueSink):
                 field:name,
                 "page": parsed_resp['page_context']['page'] + 1
             }
+            if self.config.get('organization_id'):
+                params['organization_id'] = self.config.get('organization_id')
             resp = self.request_api("GET",path,headers=headers,params=params)
             parsed_resp = json.loads(resp.content)
             records += parsed_resp[record_jsonpath]
@@ -115,8 +117,10 @@ class ZohoInventorySink(HotglueSink):
 
 
     def search_vendors(self,vendor_name):
+        result = []
         vendors = self.paginated_search("/vendors",vendor_name,"company_name.contains")
         vendor_names = [v['vendor_name'] for v in vendors]
         matches = list(self.get_close_matches(vendor_name,vendor_names,n=1,cutoff=0.8).keys())
-        result = list(filter(lambda x: x['vendor_name'] == matches[0],vendors))
+        if matches:
+            result = list(filter(lambda x: x['vendor_name'] == matches[0],vendors))
         return result
